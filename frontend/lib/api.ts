c6debd813f6a359c89dcd682aba94ebb040217d1
mahede182 <mahede.object@gmail.com>
2026-03-02 03:00:53 +0600

    fix: not found error

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import type { Job, JobsApiResponse } from '@/@types/job';
 
-const API_URL = process.env.API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
 
 type FetchJobsParams = {
     limit?: number;
